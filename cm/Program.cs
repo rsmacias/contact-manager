e2ff29e7879e92e04021473a91af69e679e9bda3
Robert Macias <Robert.Macias@trialcard.com>
2022-10-18 17:56:49 -0500

    It's alive!

--- cm/Program.cs
+++ cm/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace cm {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine("Hello Wold!");
+        }
+    }
+}
